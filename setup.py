5d7f63cc3cd862a8e237074da5b15b2dde24435f
Oscar Ortega <oscar.g.ortega.5@gmail.com>
2023-03-08 13:57:09 -0800

    adding images

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     description='A slower machine learning framework',
     url='https://github.com/oortega20/tensorslow',
     author='Oscar Ortega',
-    author_email'oscar.g.ortega.5@berkeley.edu',
+    author_email='oscar.g.ortega.5@berkeley.edu',
 )
 
