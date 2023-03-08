46c1c286a33d0fee032c1cf59cb6ec8ba21b7b62
Oscar Ortega <oscar.g.ortega.5@gmail.com>
2023-03-08 13:26:26 -0800

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='tensorslow',
+    version='1.0.0',
+    description='A slower machine learning framework',
+    url='https://github.com/oortega20/tensorslow',
+    author='Oscar Ortega',
+    author_email'oscar.g.ortega.5@berkeley.edu',
+)
+
