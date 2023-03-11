02d728127de83a86abbe6c92180895cd9bf8d4f8
Oscar Ortega <oscar.g.ortega.5@gmail.com>
2023-03-11 12:20:55 -0800

    matmul: functional

--- linalg/tensor.py
+++ linalg/tensor.py
@@ -94,7 +94,7 @@ class Tensor():
 
         
     @property
-    def transpose(self):
+    def T(self):
         transposed_shape = self.shape[:-2] + self.shape[-2:][::-1]
         tensor = Tensor([], transposed_shape)
         if self.order == 2:
@@ -111,8 +111,7 @@ class Tensor():
                     for y in range(y_dim):
                         t[y][x] = s[x][y]
         return tensor
-    
-   
+ 
     def unary_op(self, op):
         tensor = Tensor([], self.shape)
         for i in range(self.num_entries):        
@@ -122,7 +121,7 @@ class Tensor():
         return tensor
             
     
-    def binary_op(self, tensor, op):
+    def binary_op(self, op, tensor):
         ops = {
             '+': lambda x, y: x + y,
             '-': lambda x, y: x - y,
@@ -132,13 +131,13 @@ class Tensor():
         tensor = Tensor([], self.shape)
         if (self._shape_compatible(tensor, 'binary') or 
             self._shape_broadcastable(tensor, 'binary')): 
-            mod = min(self.entries, tensor.entries)
-            total_entries = max(self.entries, tensor.entries)
+            mod = min(self.num_entries, tensor.num_entries)
+            total_entries = max(self.num_entries, tensor.num_entries)
             for i in range(total_entries):
                 x = self._get_entry(self._entry_loc(i % mod))
                 y = self._get_entry(self._entry_loc(i % mod)) 
                 output = ops[op](x, y) 
-                tensor.set_entry(self._entry_loc(i), output)   
+                tensor._set_entry(self._entry_loc(i), output)   
         else:
             raise ValueError(f'''incompatible shapes for binary op: t1.shape {self.shape}, t2.shape {tensor2.shape}''')
         
@@ -146,32 +145,32 @@ class Tensor():
 
 
     def matmul(self, tensor2):
-        def dot_product(self, tensor1, tensor2):        
-            return sum(x + y for x, y in zip(tensor1, tensor2))
+        def _dot_product(tensor1, tensor2):        
+            return sum(x * y for x, y in zip(tensor1, tensor2))
  
-        def matmul_helper(tensor1, tensor2):
+        def _matmul_helper(tensor1, tensor2):
             nonlocal current_batch
             nonlocal t
             x_dim, y_dim = t.shape[-2:]
             for x in range(x_dim):
                 for y in range(y_dim):
-                    current_batch[x][y] = self._dot_product(tensor1[x], tensor2[y])
-              
+                    p)
+                    current_batch[x][y] = _dot_product(tensor1[x], tensor2[y])
+
         if self._shape_compatible(tensor2, 'matmul'):
-            t = Tensor([], self.shape[:-2] + [self.shape[-2], tensor2.shape[-1]])
-            tensor2 = tensor2.transpose
-  
+            t = Tensor([], self.shape[:-2] + self.shape[-2:-1] + tensor2.shape[-1:])  
+            tensor2 = tensor2.T
             if tensor2.order > 2:
                 for i in range(math.prod(tensor2.shape[:-2])):
                     current_batch = t._get_entry(t.entry_loc(i, is_batch=True))
                     t1 = self._get_entry(self._entry_loc(i, is_batch=True))
                     t2 = tensor2.get_entry(tensor2._entry_loc(i, is_batch=True))
-                    matmul_helper(t1, t2)
+                    _matmul_helper(t1, t2)
             else:
                 current_batch = t.tensor
-                matmul_helper(self.tensor, tensor2.tensor)
+                _matmul_helper(self.tensor, tensor2.tensor)
                 
-            tensor2 = tensor2.transpose  
+            tensor2 = tensor2.T  
             return t
    
         else:
