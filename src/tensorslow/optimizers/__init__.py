1160fa22dbc9177832f173d55ab68c8924cc1d0b
Oscar Ortega <oscar.g.ortega.5@gmail.com>
2023-03-17 11:18:49 -0700

    SGDMomentum - complete. the rest - wip

--- src/tensorslow/optimizers/__init__.py
+++ src/tensorslow/optimizers/__init__.py
@@ -1,2 +1,6 @@
 from tensorslow.optimizers.optim import Optimizer
 from tensorslow.optimizers.sgd import SGD
+from tensorslow.optimizers.sgd_momentum import SGDMomentum
+from tensorslow.optimizers.sgd_nesterov import SGDNesterov
+from tensorslow.optimizers.rms_prop import RMSProp
+from tensorslow.optimizers.adam import ADAM
