166d753e2eba74ff972491752e8e8a20fc0417ea
Oscar Ortega <oscar.g.ortega.5@gmail.com>
2023-03-18 12:41:13 -0700

    update loading ts_mnist_classifier

--- src/tensorslow/models/mnist.py
+++ src/tensorslow/models/mnist.py
@@ -1,4 +1,4 @@
-import pickle
+import dill
 import pathlib
 
 import tensorslow as ts
@@ -10,7 +10,7 @@ from tensorslow.models import Model
 
 
 img_size = 28 * 28
-x_1 = Dense('x_1', in_dim=img_size, out_dim=784)
+x_1 = Dense('x_1', in_dim=img_size, out_dim=img_size)
 a_1 = Relu()
 x_2 = Dense('x_2', in_dim=img_size, out_dim=100)
 a_2 = Relu()
@@ -22,6 +22,7 @@ ce = CrossEntropyLoss(units='bits')
 def ts_mnist_classifier(from_ts=False):
     if from_ts:
         model_path = f'{ts.__path__[0]}/saved_models/ts_mnist_classifier.pkl'
-        print(pathlib.Path(model_path))
-
+        with open(pathlib.Path(model_path), 'rb') as f:
+            model = dill.load(f)
+        return model
     return Model(x_1, a_1, x_2, a_2, x_3, s, ce)
