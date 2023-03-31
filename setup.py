194221cf9adc6ee6a353e14bf1274d16eeaaee64
Oscar Ortega <oscar.g.ortega.5@gmail.com>
2023-03-30 20:20:17 -0700

    update project readme

--- setup.py
+++ setup.py
@@ -3,16 +3,11 @@ from setuptools import setup
 if __name__ == "__main__":
     with open("README.md", "r") as fh:
         long_description = fh.read()
-    project_urls = {
-        'GitHub': "https://github.com/oortega20/tensorslow"
-    }
-
     setup(
         name='tensorslow',
         version='0.0.1',
         description=["A slower machine learning framework"],
         long_description=long_description,
         long_description_content_type="text/markdown",
-        project_urls=project_urls
     )
 
