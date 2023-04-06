eac6ba64c816c15dcabe924d03d2aa92308c4e5c
Oscar Ortega <oscar.g.ortega.5@gmail.com>
2023-04-05 22:29:45 -0700

    Added Documentation to tensorslow

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ if __name__ == "__main__":
     with open("README.md", "r") as fh:
         long_description = fh.read()
     setup(
-        name='tensorslow',
-        version='1.0.1',
-        description=["A slower machine learning framework"],
+        name="tensorslow",
+        version="1.0.1",
+        description="A slower machine learning framework",
+        author="Oscar Ortega",
+        author_email="oscar.g.ortega.5@gmail.com",
+        url="https://github.com/oortega20/tensorslow/",
         long_description=long_description,
         long_description_content_type="text/markdown",
     )
